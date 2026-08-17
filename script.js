7380bd354d40063be7617c9e01c94b98e0265e27
Etilia01 <alexandridisjulia@gmail.com>
2026-08-17 02:23:59 +0200

    the comic works now

---
+++ script.js
@@ -0,0 +1,851 @@
+
+    
+const target1= document.getElementById("count0");
+const target2= document.getElementById("count1");
+const target3= document.getElementById("count2");
+const target4= document.getElementById("count3");
+const target5= document.getElementById("count4");
+const target6= document.getElementById("count5");
+const target7= document.getElementById("count6");
+const target8= document.getElementById("count7");
+const target9= document.getElementById("count8");
+const target10= document.getElementById("count9");
+const target11= document.getElementById("count10");
+let robotsfound= 0;
+let one= 0;
+let two= 0;
+let three= 0;
+let four= 0;
+let five= 0;
+let six= 0;
+let seven= 0;
+let eight= 0;
+let nine= 0;
+let ten= 0;
+window.onload = () => {
+    target1.style.display = 'block'
+    target2.style.display = 'none'
+    target3.style.display = 'none'
+    target4.style.display = 'none'
+    target5.style.display = 'none'
+    target6.style.display = 'none'
+    target7.style.display = 'none'
+    target8.style.display = 'none'
+    target9.style.display = 'none'
+    target10.style.display = 'none'
+    target11.style.display = 'none'
+     }
+
+function toggle(){
+    if (one== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        one = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        one= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        one= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        one= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        one= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+       
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+function toggle1(){
+    if (two== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        two = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        two= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        two= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        two= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        two= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        two = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       two = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       two = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       two = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+function toggle2(){
+    if (three== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        three = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        three= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        three= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        three= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        three= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        three = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       three = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       three = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       three = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+function toggle3(){
+    if (four== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        four = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        four= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        four= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        four= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        four= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        four = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       four = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       four = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       four = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+
+function toggle4(){
+    if (five== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        five = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        five= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        five= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        five= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        five= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        five = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       five = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       five = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       five = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+
+function toggle5(){
+    if (six== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        six = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        six= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        six= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        six= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        six= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        six = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       six = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       six = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       six = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+function toggle6(){
+    if (seven== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        seven = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        seven= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        seven= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        seven= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        seven= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        seven = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       seven = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       seven = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       seven = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+function toggle7(){
+    if (eight== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        eight = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        eight= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        eight= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        eight= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        eight= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        eight = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       eight = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       eight = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       eight = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+
+function toggle8(){
+    if (nine== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        nine = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        nine= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        nine= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        nine= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        nine= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        nine = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       nine = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       nine = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       nine = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
+
+
+function toggle9(){
+    if (ten== 0) {
+    if (robotsfound==0) {
+        target2.style.display = 'block'
+        target1.style.display = 'none'
+        robotsfound = 1
+        ten = 1
+    }
+    else{
+       if (robotsfound==1) {
+        target3.style.display = 'block'
+        target2.style.display = 'none'
+        robotsfound = 2
+        ten= 1
+       } else {
+        if (robotsfound==2) {
+        target4.style.display = 'block'
+        target3.style.display = 'none'
+        robotsfound = 3  
+        ten= 1
+        } else {
+        if (robotsfound==3) {
+        target5.style.display = 'block'
+        target4.style.display = 'none'
+        robotsfound = 4 
+        ten= 1 
+        } else {
+          if (robotsfound==4) {
+        target6.style.display = 'block'
+        target5.style.display = 'none'
+        robotsfound = 5 
+        ten= 1 
+        } else {
+          if (robotsfound==5) {
+        target7.style.display = 'block'
+        target6.style.display = 'none'
+        robotsfound = 6
+        ten = 1
+       } 
+       else {
+          if (robotsfound==6) {
+        target8.style.display = 'block'
+        target7.style.display = 'none'
+        robotsfound = 7
+       ten = 1
+       }
+       else {
+          if (robotsfound==7) {
+        target9.style.display = 'block'
+        target8.style.display = 'none'
+        robotsfound = 8
+       ten = 1
+       } 
+       else {
+          if (robotsfound==8) {
+        target10.style.display = 'block'
+        target9.style.display = 'none'
+        robotsfound = 9
+       ten = 1
+       } else {
+        if (robotsfound==9){window.location.href = "page3.html";}
+        
+       
+       }
+       
+       
+       } 
+       
+       }
+       
+       }
+       } 
+       
+       }
+       }
+     
+    }
+}
+}
+}
